44fd89e921df32e3b70dc28546efecb964564641
Bojan Petkovic <bojan.petkovic@asioso.com>
2024-07-27 21:19:23 +0200

    full backend changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,33 +1,50 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import config from './config';
 import userRoutes from './routes/userRoutes';
 import authRoutes from './routes/authRoutes';
-
-// Load environment variables
-dotenv.config();
+import mongoSanitize from 'express-mongo-sanitize';
+import rateLimit from 'express-rate-limit';
+import errorHandler from './middleware/errorHandler';
+import morgan from 'morgan';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const { port, mongoURI } = config;
+
+// Rate limiting middleware
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
+
+// Middleware to sanitize user input
+app.use(mongoSanitize());
 
 app.use(express.json());
 
-app.use('/api', userRoutes);
-app.use('/api/auth', authRoutes);
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGO_URI!)
+  .connect(mongoURI)
   .then(() => {
     console.log('Connected to MongoDB Atlas');
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
     });
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB Atlas:', error);
   });
+
+app.use('/api/auth', authRoutes); // Public routes for registration and login
+app.use('/api', userRoutes); // Protected routes
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Error handler middleware
+app.use(errorHandler);
+app.use(morgan('combined'));
